d77c08d959d1b922562edca30b99f1c533999c5d
Daniel Kayiwa <dkayiwa@openmrs.org>
2011-05-31 10:04:02 +0000

    Fixing bug in standalone app where log file name was incorrect - STAND-1

--- src/main/java/org/openmrs/standalone/TextAreaWriter.java
+++ src/main/java/org/openmrs/standalone/TextAreaWriter.java
@@ -71,7 +71,7 @@ public class TextAreaWriter extends FilterOutputStream {
 		//Append to the log file under currentdir/tomcat/logs
 		try {
 			Calendar cal = Calendar.getInstance();
-			String fileName = cal.get(Calendar.DATE) + "-" + cal.get(Calendar.MONTH) + 1 + "-" + cal.get(Calendar.YEAR)
+			String fileName = cal.get(Calendar.DATE) + "-" + (cal.get(Calendar.MONTH) + 1) + "-" + cal.get(Calendar.YEAR)
 			+ ".log";
 			String path = new File(fileName).getAbsolutePath();
 			path = path.substring(0, path.lastIndexOf(File.separatorChar) + 1) + "tomcat" + File.separatorChar + "logs";
