832f171bea78a1305b4ed7b0b85ca1e4543491ff
deeno <bajideen@gmail.com>
2024-04-27 20:53:12 +0530

    STAND-108:ENHC:Introduced slf4j logger into the project, also as the first step logger was added into OpenmrsUtil.java class

--- src/main/java/org/openmrs/standalone/OpenmrsUtil.java
+++ src/main/java/org/openmrs/standalone/OpenmrsUtil.java
@@ -21,9 +21,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class OpenmrsUtil {
-	
+	private OpenmrsUtil(){}
 	private static final String OPERATING_SYSTEM_KEY = "os.name";
 	
 	private static final String OPERATING_SYSTEM = System.getProperty(OPERATING_SYSTEM_KEY);
@@ -37,16 +38,19 @@ public class OpenmrsUtil {
 	private static final String OPERATING_SYSTEM_OSX = "Mac OS X";
 	
 	private static final String OPERATING_SYSTEM_WINDOWS_DUMMY = "Windows 7";
+	private static final String MY_SQL_PORT = "3316";
 	
 	private static String runtimePropertiesPathName;
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(OpenmrsUtil.class);
+
 	/**
 	 * Shortcut booleans used to make some OS specific checks more generic; note the *nix flavored
 	 * check is missing some less obvious choices
 	 */
-	private static final boolean UNIX_BASED_OPERATING_SYSTEM = (OPERATING_SYSTEM.indexOf(OPERATING_SYSTEM_LINUX) > -1
-			|| OPERATING_SYSTEM.indexOf(OPERATING_SYSTEM_SUNOS) > -1
-			|| OPERATING_SYSTEM.indexOf(OPERATING_SYSTEM_FREEBSD) > -1 || OPERATING_SYSTEM.indexOf(OPERATING_SYSTEM_OSX) > -1);
+	private static final boolean UNIX_BASED_OPERATING_SYSTEM = (OPERATING_SYSTEM.contains(OPERATING_SYSTEM_LINUX)
+			|| OPERATING_SYSTEM.contains(OPERATING_SYSTEM_SUNOS)
+			|| OPERATING_SYSTEM.contains(OPERATING_SYSTEM_FREEBSD) || OPERATING_SYSTEM.contains(OPERATING_SYSTEM_OSX));
 	
 	
 	
@@ -68,63 +72,66 @@ public class OpenmrsUtil {
 	 */
 
 	public static Properties getRuntimeProperties(String applicationName) {
-		
+
 		setDummyOS();
 		if (applicationName == null)
 			applicationName = "openmrs";
-		
+
 		FileInputStream propertyStream = null;
-		
+
 		String filename = applicationName + "-runtime.properties";
 		// first look in the current directory (that java was started from)
 		runtimePropertiesPathName = filename;
-		System.out.println("Attempting to load properties file in current directory: " + runtimePropertiesPathName);
+		LOGGER.info("Attempting to load properties file in current directory: {}", runtimePropertiesPathName);
 		try {
 			propertyStream = new FileInputStream(runtimePropertiesPathName);
 		}
 		catch (FileNotFoundException e) {
+			LOGGER.error("File not found in the provided path {}", runtimePropertiesPathName);
 		}
-		
+
 		// next look for an environment variable
 		if (propertyStream == null) {
 			String envVarName = applicationName.toUpperCase() + "_RUNTIME_PROPERTIES_FILE";
 			runtimePropertiesPathName = System.getenv(envVarName);
 			if (runtimePropertiesPathName != null) {
-				System.out.println("Atempting to load runtime properties from: " + runtimePropertiesPathName);
+				LOGGER.info("Attempting to load runtime properties from: {}", runtimePropertiesPathName);
 				try {
 					propertyStream = new FileInputStream(runtimePropertiesPathName);
 				}
 				catch (IOException e) {
+					LOGGER.error("An exception occurred while attempting to load runtime properties from: {}", runtimePropertiesPathName, e);
 				}
 			}
 		}
-		
+
 		// next look in the OpenMRS application data directory
 		if (propertyStream == null) {
 			runtimePropertiesPathName = OpenmrsUtil.getApplicationDataDirectory() + filename;
-			System.out.println("Attempting to load property file from: " + runtimePropertiesPathName);
+			LOGGER.info("Attempting to load property file from: {}", runtimePropertiesPathName);
 			try {
 				propertyStream = new FileInputStream(runtimePropertiesPathName);
 			}
 			catch (FileNotFoundException e) {
+				LOGGER.error("File not found in the provided path: {}", runtimePropertiesPathName);
 			}
 		}
-		
+
 		try {
 			if (propertyStream != null) {
 				Properties props = new Properties();
 				loadProperties(props, propertyStream);
 				propertyStream.close();
-				System.out.println("Using runtime properties file: " + runtimePropertiesPathName);
+				LOGGER.info("Using runtime properties file: {}", runtimePropertiesPathName);
 				return props;
 			}
 		}
 		catch (Exception ex) {
-			ex.printStackTrace();
+			LOGGER.error("An error occurred while loading properties file: {}", ex.getMessage(), ex);
 		}
-		
-		System.out.println("Failed to get runtime properties file.");
-		
+
+		LOGGER.error("Failed to get runtime properties file.");
+
 		return null;
 		
 		/*if (applicationName == null)
@@ -200,7 +207,7 @@ public class OpenmrsUtil {
 	 * </pre>
 	 * 
 	 * @return The path to the directory on the file system that will hold miscellaneous data about
-	 *         the application (runtime properties, modules, etc)
+	 *         the application (runtime properties, modules, etc.)
 	 */
 	private static String getApplicationDataDirectory() {
 		
@@ -225,21 +232,22 @@ public class OpenmrsUtil {
 	 * Convenience method used to load properties from the given file.
 	 * 
 	 * @param props the properties object to be loaded into
-	 * @param propertyFile the properties file to read
+	 * @param inputStream the input stream to intialize the input stream reader
 	 */
 	private static void loadProperties(Properties props, InputStream inputStream) {
 		try {
 			InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
 			props.load(reader);
+			LOGGER.info("Loading properties was Successful");
 		}
 		catch (FileNotFoundException fnfe) {
-			System.out.println("Unable to find properties file" + fnfe);
+			LOGGER.error("Unable to find properties file", fnfe);
 		}
 		catch (UnsupportedEncodingException uee) {
-			System.out.println("Unsupported encoding used in properties file" + uee);
+			LOGGER.error("Unsupported encoding used in properties file", uee);
 		}
 		catch (IOException ioe) {
-			System.out.println("Unable to read properties from properties file" + ioe);
+			LOGGER.error("Unable to read properties from properties file", ioe);
 		}
 		finally {
 			try {
@@ -247,17 +255,17 @@ public class OpenmrsUtil {
 					inputStream.close();
 			}
 			catch (IOException ioe) {
-				System.out.println("Unable to close properties file " + ioe);
+				LOGGER.error("Unable to close properties file", ioe);
 			}
 		}
 	}
-	
+
 	public static String getRuntimePropertiesPathName(){
 		return runtimePropertiesPathName;
 	}
 	
 	public static String getMysqlPort(){
-		return "3316";
+		return MY_SQL_PORT;
 	}
 	public static void setDummyOS(){
 		if(!UNIX_BASED_OPERATING_SYSTEM){
